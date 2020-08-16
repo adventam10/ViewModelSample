ab0a6f5aaa78a3e29807bfbe4a347c71cf592765
am10 <adventam10@gmail.com>
2020-08-17 01:24:20 +0900

    Fix ViewModel factory method

--- ViewModelSample/app/src/main/java/com/example/viewmodelsample/MainFragment.kt
+++ ViewModelSample/app/src/main/java/com/example/viewmodelsample/MainFragment.kt
@@ -1,14 +1,15 @@
 package com.example.viewmodelsample
 
 import android.content.Intent
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import com.example.viewmodelsample.databinding.MainFragmentBinding
 
 
@@ -18,14 +19,14 @@ class MainFragment : Fragment() {
         fun newInstance() = MainFragment()
     }
 
-    private lateinit var viewModel: MainViewModel
+    private val viewModel: MainViewModel by viewModels()
     private lateinit var dataBinding: MainFragmentBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
+//        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
         dataBinding = MainFragmentBinding.inflate(inflater, container, false).also {
             it.lifecycleOwner = this.viewLifecycleOwner
             it.viewModel = this.viewModel
